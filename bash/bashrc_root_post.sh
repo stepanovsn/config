a66014282bb4ee5c9b881e498dac7ca42b02add9
regular-xubuntu <inndie.md@gmail.com>
2021-08-12 01:37:46 +0300

    improve bash section

--- bash/bashrc_root_post.sh
+++ bash/bashrc_root_post.sh
@@ -5,3 +5,5 @@ export PS1='${debian_chroot:+($debian_chroot)}\[\033[31m\]\u@\h\[\033[00m\]\[\03
 
 # set up LS colors
 export LS_COLORS='rs=0:di=34:ln=36:mh=00:pi=35:so=35:do=35:bd=35:cd=35:or=1;30:mi=1;30:su=37:sg=37:ca=30;41:tw=34:ow=34:st=34:ex=33:fi=37:';
+
+export EDITOR=nvim

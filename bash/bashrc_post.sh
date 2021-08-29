7d04dde681f5b7c82750a8fcfb847f296ca51c45
regular-xubuntu <inndie.md@gmail.com>
2021-08-30 00:31:58 +0300

    add alias, reorganize files, add notes

--- bash/bashrc_post.sh
+++ bash/bashrc_post.sh
@@ -7,3 +7,6 @@ export PS1='${debian_chroot:+($debian_chroot)}\[\033[32m\]\u@\h\[\033[00m\]\[\03
 export LS_COLORS='rs=0:di=34:ln=36:mh=00:pi=35:so=35:do=35:bd=35:cd=35:or=1;30:mi=1;30:su=37:sg=37:ca=30;41:tw=34:ow=34:st=34:ex=33:fi=37:';
 
 export EDITOR=nvim
+
+# aliases
+alias r.du='du -ah --max-depth=1 | sort -h'

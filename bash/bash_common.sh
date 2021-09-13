7a80a3bb3e98fb32488993c09e34fa1774ebd567
Sergey Stepanov <sergey.stepanov@kaspersky.com>
2021-09-13 15:31:36 +0300

    fix .rgrc path

--- bash/bash_common.sh
+++ bash/bash_common.sh
@@ -4,7 +4,7 @@
 export LS_COLORS='rs=0:di=34:ln=36:mh=00:pi=35:so=35:do=35:bd=35:cd=35:or=1;30:mi=1;30:su=37:sg=37:ca=30;41:tw=34:ow=34:st=34:ex=33:fi=37:';
 export EDITOR=nvim
 export FZF_DEFAULT_OPTS="-e"
-export RIPGREP_CONFIG_PATH=".rgrc"
+export RIPGREP_CONFIG_PATH=$HOME"/.rgrc"
 
 # aliases
 alias ncdu='ncdu -r'

80e16de847806c87102036a404e1f2035f1cec31
Sergey Stepanov <sergey.stepanov@kaspersky.com>
2021-09-22 12:50:58 +0300

    add more preview options for lf

--- lf/preview.sh
+++ lf/preview.sh
@@ -6,10 +6,9 @@ case "$1" in
     *.zip) unzip -l "$1";;
     *.rar) unrar l "$1";;
     *.7z) 7z l "$1";;
-    #*.pdf) pdftotext "$1" -;;
-    #*.png|*.jpg|*.jpeg|*.mkv|*.mp4|*.m4v) mediainfo "$1";;
-    #*.md) glow -s dark "$1";;
-    #*.html|*.xml) w3m -dump "$1";;
-    *.pem) openssl x509 -in "$1" -noout -text;;
+    *.pdf) pdftotext "$1" -;;
+    *.png|*.jpg|*.jpeg|*.mkv|*.mp4|*.m4v) mediainfo "$1";;
+    *.html|*.xml) w3m -dump "$1";;
+    *.pem|*.crt|*.cer) openssl x509 -in "$1" -noout -text -certopt no_pubkey,no_sigdump;;
     *) highlight -O ansi "$1";;
 esac

70c62a6c50f8cb653c660c43f08e815e628b1407
regular-xubuntu <inndie.md@gmail.com>
2021-09-23 00:34:08 +0500

    add lf preview options

--- lf/preview.sh
+++ lf/preview.sh
@@ -3,6 +3,8 @@
 case "$1" in
     *.tar) tar -tvf "$1";;
     *.tar.gz) tar -tvzf "$1";;
+    *.tar.bz) tar -tvjf "$1";;
+    *.tar.xz) tar -tvJf "$1";;
     *.zip) unzip -l "$1";;
     *.rar) unrar l "$1";;
     *.7z) 7z l "$1";;
@@ -10,5 +12,6 @@ case "$1" in
     *.png|*.jpg|*.jpeg|*.mkv|*.mp4|*.m4v) mediainfo "$1";;
     *.html|*.xml) w3m -dump "$1";;
     *.pem|*.crt|*.cer) openssl x509 -in "$1" -noout -text -certopt no_pubkey,no_sigdump;;
+    *.deb) dpkg-deb -I "$1";;
     *) highlight -O ansi "$1";;
 esac

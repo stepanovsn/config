90421c150c7cedb489c97a82b2eb9f6b14dbdb9f
regular-xubuntu <inndie.md@gmail.com>
2022-05-27 03:26:07 +0500

    bash: add umask; add reload_audio function; add tmux launch on startup

--- bash/bash_common.sh
+++ bash/bash_common.sh
@@ -1,6 +1,7 @@
 # bash
 
 set -o ignoreeof
+umask 077
 
 # set up LS colors
 export LS_COLORS='rs=0:di=34:ln=36:mh=00:pi=35:so=35:do=35:bd=35:cd=35:or=1;30:mi=1;30:su=37:sg=37:ca=30;41:tw=34:ow=34:st=34:ex=33:fi=37:';
@@ -49,6 +50,10 @@ r.testcolor() {
     printf "\x1b[38;2;255;100;0mTRUECOLOR\x1b[0m\n"
 }
 
+r.reload_audio() {
+    pulseaudio -k && sudo alsa force-reload
+}
+
 # set up lf
 LFCD=$HOME"/.config/lf/lfcd.sh"
 if [ -f "$LFCD" ]; then
@@ -60,3 +65,19 @@ bind '"\C-n":"lfcd\C-m"'
 bind '"\C-p":"fzf\C-m"'
 bind '"\C-h":"nvim\C-m"'
 bind '"\C-]":" | nvim -"'
+
+# launch tmux session
+# there should be main.sh in home directory
+if $(command -v tmux &> /dev/null) && ! [ -n "$TMUX" ]; then
+    if tmux ls &> /dev/null; then
+        tmux attach
+    else
+        cd ~/
+        FILE=./tmux_main.sh
+        if [ -f "$FILE" ]; then
+            ./tmux_main.sh
+        else
+            tmux new -s main
+        fi
+    fi
+fi

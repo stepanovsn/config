6f6b5eb14b0209795df962980efa502237293a05
regular-xubuntu <inndie.md@gmail.com>
2022-06-03 02:52:31 +0500

    internal: add pre-check for clean repo

--- install_console.sh
+++ install_console.sh
@@ -4,9 +4,16 @@ ROOT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]:-$0}";)" &> /dev/null && pwd 2
 source $ROOT_DIR/settings.sh
 ROOT_DIR=$(minimize_path "${ROOT_DIR}")
 
-# Run tasks
-printf "\n"
+# Check that repository is clean
+printf "${cYellow}Pre-check${cNone}\n"
 
+if [[ "$(git status --porcelain)" ]]; then
+    printf "\tThe config repo is not clean.\n\t${cRed}Failed.${cNone}\n\n"
+    exit 1
+fi
+printf "\t${cGreen}Done.${cNone}\n\n"
+
+# Run tasks
 source $ROOT_DIR/internal/install.sh
 source $ROOT_DIR/bash/install.sh
 source $ROOT_DIR/other/install.sh

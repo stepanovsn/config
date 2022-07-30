b4c81add5f1a965cfcd02d8d3ee8c4cdd50c6fa6
Sergey Stepanov <inndie.md@gmail.com>
2022-07-31 02:55:22 +0500

    xterm: move installation to other_gui

--- other_gui/install.sh
+++ other_gui/install.sh
@@ -2,9 +2,9 @@
 
 install_other_gui() {
     if distr_arch; then
-        step_upgrade_pacman autorandr picom
+        step_upgrade_pacman autorandr picom xterm
     else
-        step_upgrade_apt autorandr picom
+        step_upgrade_apt autorandr picom xterm
     fi
 
     local font_dir="/usr/share/fonts/googlefonts"
@@ -17,10 +17,12 @@ install_other_gui() {
     step_print "Roboto Mono font installed."
 
     step_soft_link $ROOT_DIR/other_gui/.xinitrc $HOME/.xinitrc
-    step_print ".xinitrc link made."
-
     step_soft_link $ROOT_DIR/other_gui/.xprofile $HOME/.xprofile
-    step_print ".xprofile link made."
+    step_soft_link $ROOT_DIR/other_gui/.Xresources $HOME/.Xresources
+    step_print "Config links made."
+
+    xrdb -merge ~/.Xresources
+    step_print "Xresources merged."
 
     step_done
 }

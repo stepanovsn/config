7b1f7fd0066dee6881137ed1056991dddcbccdd6
Sergey Stepanov <inndie.md@gmail.com>
2022-09-16 03:35:53 +0500

    bash: save REG_MACHINE export variable

--- bash/install.sh
+++ bash/install.sh
@@ -6,17 +6,25 @@ install_bash() {
     local bashrc_updated=0
     local bashrc=$HOME/.bashrc
     local bash_source_text="source ${ROOT_DIR}/bash/bashrc_post.sh"
-    local distr_text="export REG_DISTR=\"${REG_DISTR}\""
+    local distr_text="export REG_DISTRO=\"${REG_DISTRO}\""
+    local machine_text="export REG_MACHINE=\"${REG_MACHINE}\""
 
     if [ ! -f $bashrc ]; then
         printf "#!/bin/bash\n" >> $bashrc
-        step_warn "$bashrc not found. Default one created."
+        step_warn "$bashrc not found. Default one will be created."
         local message_shown=1
     fi
 
     if ! grep -xq "${distr_text}" $bashrc; then
         printf "\n# Specify Linux distribution:\n${distr_text}\n" >> $bashrc
         local bashrc_updated=1
+    fi
+
+    if [ ! -z ${REG_MACHINE} ]; then
+        if ! grep -xq "${machine_text}" $bashrc; then
+            printf "\n# Specify machine profile:\n${machine_text}\n" >> $bashrc
+            local bashrc_updated=1
+        fi
     fi
 
     if ! grep -xq "${bash_source_text}" $bashrc; then
@@ -40,7 +48,7 @@ install_bash() {
 
     if [ -z $(sudo find /root -maxdepth 1 -name .bashrc) ]; then
         printf "#!/bin/bash\n" | sudo tee $bashrc > /dev/null
-        step_warn "$bashrc not found. Default one created."
+        step_warn "$bashrc not found. Default one will be created."
         local message_shown=1
     fi
 

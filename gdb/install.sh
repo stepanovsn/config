1806ceca8b9f8819e4447c94381f44ffef667b4b
Sergey Stepanov <inndie.md@gmail.com>
2022-10-27 01:00:23 +0500

    gdb: install the package

--- gdb/install.sh
+++ gdb/install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 install_gdb() {
+    if distro_arch; then
+        step_upgrade_pacman gdb
+    else
+        step_upgrade_apt gdb
+    fi
+
     local gdb_dir=$HOME/.gdbinit.d
     rm -rf $gdb_dir && mkdir $gdb_dir
     step_soft_link $ROOT_DIR/gdb/.gdbinit $HOME/.gdbinit

ef9756687d786a44212df3cff64e17fe35b2b77d
Sergey Stepanov <inndie.md@gmail.com>
2022-12-07 01:24:26 +0500

    bluetooth: add support

--- system/install.sh
+++ system/install.sh
@@ -3,14 +3,17 @@
 install_system() {
     local packages=(
         "alsa-utils"
+        "bluez-utils"
         "firefox"
         "pulseaudio"
         "pulseaudio-alsa"
+        "pulseaudio-bluetooth"
         "pulseaudio-jack"
         "slock"
         "telegram-desktop")
 
     step_upgrade_pacman ${packages[@]}
+    step_service_activate bluetooth
     step_install_snap stellarium-daily
     step_done
 }

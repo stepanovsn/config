8349789a7c502b958644e533049f866d1ebdfd66
Sergey Stepanov <inndie.md@gmail.com>
2022-12-28 19:38:56 +0500

    bash: add HISTCONTROL variable

--- bash/bashrc_common.sh
+++ bash/bashrc_common.sh
@@ -33,6 +33,7 @@ ls_colors="rs=0:di=34:ln=36:mh=00:pi=35:so=35:do=35:bd=35:cd=35:or=1;\
 30:mi=1;30:su=37:sg=37:ca=30;41:tw=34:ow=34:st=34:ex=33:fi=37:";
 export LS_COLORS="$ls_colors"
 export EDITOR=nvim
+export HISTCONTROL=ignoreboth
 export FZF_DEFAULT_OPTS="-e"
 
 # Aliases

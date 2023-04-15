93e6f77f89dc6d1194071bbcb5ced2a3a0623d96
Sergey Stepanov <inndie.md@gmail.com>
2023-04-15 16:46:39 +0500

    grub: add minor message

--- grub/install.sh
+++ grub/install.sh
@@ -7,7 +7,7 @@ install_grub() {
     else
         local vt_colors="vt.default_red=24,201,81,209,73,187,66,200,85,222,55,227,50,224,50,255 vt.default_grn=24,87,196,162,116,56,207,200,85,58,212,209,107,31,219,255 vt.default_blu=24,79,106,69,191,194,200,200,85,47,89,52,207,215,211,255"
         if ! grep -q "${vt_colors}" $grub_file; then
-            step_failed "Vt colors are outdated"
+            step_failed "Vt colors are outdated. Update the colors and remake grub config"
         else
             step_print "Vt colors are up-to-date"
         fi

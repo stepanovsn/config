709554b690a856534886eb41fa78cf638bf6b895
Sergey Stepanov <inndie.md@gmail.com>
2023-04-15 16:48:38 +0500

    grub: minor improvement in script

--- grub/install.sh
+++ grub/install.sh
@@ -4,14 +4,13 @@ install_grub() {
     local grub_file="/etc/default/grub"
     if ! [ -f $grub_file ]; then
         step_failed "$grub_file not found"
-    else
-        local vt_colors="vt.default_red=24,201,81,209,73,187,66,200,85,222,55,227,50,224,50,255 vt.default_grn=24,87,196,162,116,56,207,200,85,58,212,209,107,31,219,255 vt.default_blu=24,79,106,69,191,194,200,200,85,47,89,52,207,215,211,255"
-        if ! grep -q "${vt_colors}" $grub_file; then
-            step_failed "Vt colors are outdated. Update the colors and remake grub config"
-        else
-            step_print "Vt colors are up-to-date"
-        fi
     fi
+
+    local vt_colors="vt.default_red=24,201,81,209,73,187,66,200,85,222,55,227,50,224,50,255 vt.default_grn=24,87,196,162,116,56,207,200,85,58,212,209,107,31,219,255 vt.default_blu=24,79,106,69,191,194,200,200,85,47,89,52,207,215,211,255"
+    if ! grep -q "${vt_colors}" $grub_file; then
+        step_failed "Vt colors are outdated. Update the colors and remake grub config"
+    fi
+    step_print "Vt colors are up-to-date"
 
     step_done
 }

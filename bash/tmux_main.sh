228b363d625c2edd595cc8b6c400675f6bb6ed49
Sergey Stepanov <inndie.md@gmail.com>
2024-11-11 17:24:10 +0500

    tmux: update tmux_main.sh

--- bash/tmux_main.sh
+++ bash/tmux_main.sh
@@ -6,9 +6,29 @@ send-keys C-l \; \
 new-window -n 'materials' \; \
 send-keys 'cd ~/materials' C-m \; \
 send-keys C-l \; \
+split-window -h -l 50 \; \
+send-keys 'cd ~/materials' C-m './watch.sh'  C-m \; \
+select-pane -t 0 \; \
+resize-pane -Z \; \
+new-window -n 'rnote' \; \
+send-keys 'cd ~/rn_converter' C-m \; \
+send-keys C-l \; \
+split-window -h -l 50 \; \
+send-keys 'cd ~/rn_converter' C-m './watch.sh'  C-m \; \
+select-pane -t 0 \; \
+resize-pane -Z \; \
+new-window -n 'wordstock' \; \
+send-keys 'cd ~/wordstock' C-m \; \
+send-keys C-l \; \
+split-window -h -l 50 \; \
+send-keys 'cd ~/wordstock' C-m './watch.sh'  C-m \; \
+select-pane -t 0 \; \
+resize-pane -Z \; \
 new-window -n 'other' \; \
-select-window -t 3 \;
 
 # For window splitting
 #split-window -h -l 40 \; \
 #select-pane -t 0 \; \
+
+# For selecting startup window
+#select-window -t 3 \;

55a1fb65e0db38f44c501c72f796cacc60f43409
Sergey Stepanov <inndie.md@gmail.com>
2025-10-25 02:59:45 +0500

    nvim: custom tabline

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -46,3 +46,73 @@ vim.opt.grepprg = 'rg --vimgrep'
 
 -- Syntax
 vim.cmd('match ExtraWhitespace /\\s\\+$/')
+
+-- Tabline
+function _G.my_tabline()
+    local current_tab = vim.fn.tabpagenr()
+    local total_tabs = vim.fn.tabpagenr('$')
+    local tabline = ''
+
+    local extra_tabs_on_right = current_tab < 4 and 4 - current_tab or 0
+    local extra_tabs_on_left = current_tab + 3 > total_tabs and current_tab + 3 - total_tabs or 0
+
+    local first_tab_to_display = current_tab - 3 - extra_tabs_on_left
+    local last_tab_to_display = current_tab + 3 + extra_tabs_on_right
+
+    local show_dots_at_start = first_tab_to_display > 1 and true or false
+    local show_dots_at_end = last_tab_to_display < total_tabs and true or false
+
+    local first_tab_to_display = math.max(first_tab_to_display, 1)
+    local last_tab_to_display = math.min(last_tab_to_display, total_tabs)
+
+    if show_dots_at_start then
+        tabline = tabline .. '%#TabLine#... '
+    end
+
+    for i = first_tab_to_display, last_tab_to_display do
+        local buflist = vim.fn.tabpagebuflist(i)
+        local winnr = vim.fn.tabpagewinnr(i)
+        local bufnr = buflist[winnr]
+        local bufname = vim.fn.bufname(bufnr)
+        local filetype = vim.fn.getbufvar(bufnr, '&filetype')
+
+        local is_current = i == current_tab
+
+        tabline = tabline .. '%' .. i .. 'T'
+        tabline = tabline .. (is_current and '%#TabLineSel#' or '%#TabLine#')
+        tabline = tabline ..  ' ' .. i .. ':'
+
+        tabline = tabline .. get_custom_tab_name(bufname, filetype, bufnr)
+
+        if vim.fn.getbufvar(bufnr, '&modified') == 1 then
+            tabline = tabline .. ' [+]'
+        end
+
+        tabline = tabline .. ' %T'
+    end
+
+    if show_dots_at_end then
+        tabline = tabline .. '%#TabLine# ...'
+    end
+
+    local cwd = vim.fn.getcwd()
+    cwd = cwd:gsub('^' .. vim.env.HOME, '~')
+    if #cwd > 30 then
+        cwd = '..' .. cwd:sub(-29)
+    end
+
+    tabline = tabline .. '%#TabLineFill#%= ' .. cwd .. ' '
+    return tabline
+end
+
+function get_custom_tab_name(bufname, filetype, bufnr)
+    if bufname == '' then
+        return '[noname]'
+    elseif filetype == 'fugitive' then
+        return '[git]'
+    else
+        return vim.fn.fnamemodify(bufname, ':t')
+    end
+end
+
+vim.opt.tabline = "%!v:lua.my_tabline()"
